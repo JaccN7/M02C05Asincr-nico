5e0e3e71c8135b8d1f6ea99b7459930c57c48a82
Javiera Cortés <javieracortesceledon@gmail.com>
2022-04-28 02:25:39 -0400

    M02C05 Asincrónico Agregado

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function sumarArreglo() {
+    var cantNumeros = parseInt(document.getElementById("cantNumeros").value);
+    var arreglo = [];
+    var sumarArreglo = 0;
+    if (cantNumeros <= 1) {
+        alert("Para poder realizar la operación es necesario que indiques que se sumaran mas de un número")
+    } else {
+        for (var i = 0; i < cantNumeros; i++) {
+            arreglo[i] = parseInt(prompt("Ingresa un NÚMERO para sumar: "));
+            console.log("Valor en posición " + (i + 1) + ": " + arreglo[i]);
+        }
+        console.log("Este es el array creado: arreglo[" + arreglo + "]");
+
+        arreglo.forEach(arregloNum => {
+            sumarArreglo += arregloNum;
+        });
+
+        console.log(sumarArreglo)
+        document.getElementById("aquiRespuesta").innerHTML = ('<p color="blue">La suma de los numeros que contiene arreglo[' + arreglo + '] es: ' + sumarArreglo);
+    }
+}
